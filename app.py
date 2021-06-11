6d337098d3652019c33340282c559053ccd665bb
Rebecca Bland <rebeccaannebland@gmail.com>
2021-06-10 20:04:13 -0700

    updated files

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 # from models import create_classes
 import os
 from flask import Flask, render_template, jsonify, request, redirect
-from sqlalchemy import create_engine
 import pandas as pd
+
+
 
 #################################################
 # Flask Setup
@@ -14,26 +15,14 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-from flask_sqlalchemy import SQLAlchemy
-
-
-# # db = SQLAlchemy(app)
-db = create_engine("sqlite:///data/species_database.sqlite")
-
 # create route that renders index.html template
 @app.route("/")
 def home():
-
     return render_template("index.html")
 
-@app.route("/api/tracks")
-def species():
-    sql = "select * from tracks"
-    species_df = pd.read_sql(sql = sql, con = db)
-
-    species_results = species_df.to_json(orient = "records")
-    
-    return jsonify(species_results)
+@app.route("/visualizations")
+def visualizations():
+    return render_template("visualizations.html")
 
 if __name__ == "__main__":
     app.run()
